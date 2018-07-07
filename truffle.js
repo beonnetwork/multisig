fc437695d8be9944e3bda38f2024a50d19470a2e
Surat Teerapittayanon <steerapi@gmail.com>
2018-07-07 22:57:52 +0700

    Update truffle config

--- truffle.js
+++ truffle.js
@@ -1,10 +1,39 @@
+require("dotenv").config();
+const LedgerWalletProvider = require("truffle-ledger-provider");
+
 module.exports = {
   networks: {
+    mainnet: {
+      provider: new LedgerWalletProvider({
+          path: "44'/60'/0'/0", // ledger default derivation path
+          askConfirm: false,
+          accountsLength: 1,
+          networkId: 3, // ropsten testnet
+          accountsOffset: 0 // we use the first address
+        },
+        `https://mainnet.infura.io/${process.env.INFURA_APIKEY}`
+      ),
+      network_id: 3,
+      gas: 4700000,
+    },
+    ropsten: {
+      provider: new LedgerWalletProvider({
+          path: "44'/60'/0'/0", // ledger default derivation path
+          askConfirm: false,
+          accountsLength: 1,
+          networkId: 3, // ropsten testnet
+          accountsOffset: 0 // we use the first address
+        },
+        `https://ropsten.infura.io/${process.env.INFURA_APIKEY}`
+      ),
+      network_id: 3,
+      gas: 4700000,
+    },
     development: {
       host: "localhost",
-      port: 7545,  
+      port: 7545,
       network_id: 6777,
       gas: 8000000,
     },
-  },
+  }
 };
